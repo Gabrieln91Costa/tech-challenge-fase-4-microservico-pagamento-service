893f5f7cc084a7d21e7d31bc5ff42b6063785074
Gabriel <gabrieln91@hotmail.com>
2025-06-03 00:17:24 -0300

    Subindo Arquivos!!

--- pagamento-service/src/main/java/com/microservico/pagamentoservice/infrastructure/messaging/PagamentoConsumer.java
+++ pagamento-service/src/main/java/com/microservico/pagamentoservice/infrastructure/messaging/PagamentoConsumer.java
@@ -1,26 +1,34 @@
 package com.microservico.pagamentoservice.infrastructure.messaging;
 
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Component;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microservico.pagamentoservice.application.usecase.CriarPagamento;
 import com.microservico.pagamentoservice.domain.model.Pagamento;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
 
 @Component
 public class PagamentoConsumer {
 
     private final CriarPagamento criarPedido;
+    private final ObjectMapper objectMapper = new ObjectMapper(); // usado para exibir JSON bonito
 
     public PagamentoConsumer(CriarPagamento criarPedido) {
         this.criarPedido = criarPedido;
     }
 
-    @RabbitListener(queues = "fila.pedidos") // mesma fila usada pelo pedidoreceiver
+    @RabbitListener(queues = "fila.pedidos")
     public void receberPedido(Pagamento pedido) {
-        System.out.println("📩 Pedido recebido do RabbitMQ no PedidoService:");
-        System.out.println(pedido);
+        System.out.println("📩 Pedido de Pagamento recebido do RabbitMQ do PedidoService:");
 
-        // Processar o pedido (salvar, verificar estoque, etc.)
-        criarPedido.criarPagamento(pedido);
+        try {
+            String jsonFormatado = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(pedido);
+            System.out.println(jsonFormatado); // saída em JSON bonitinho
+        } catch (JsonProcessingException e) {
+            System.out.println("Erro ao exibir pagamento como JSON. Exibindo toString:");
+            System.out.println(pedido); // fallback
+        }
+
+        // criarPedido.criarPagamento(pedido);
     }
 }
